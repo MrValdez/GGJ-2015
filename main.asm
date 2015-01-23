b062be2ea2914cb8de050a918829426ad98578a5
MrValdez <shunyvaldez@gmail.com>
2015-01-24 03:16:10 +0800

    added left-right walk cycle for golem

--- main.asm
+++ main.asm
@@ -42,17 +42,17 @@ Main:
     PrintString "\n\n"
 
 ; green background
-;    sep #$20
-;    lda #%10000000
-;    sta $2100
+    sep #$20
+    lda #%10000000
+    sta $2100
     
-;    lda     #%11100000  ; Load the low byte of the green color.
-;    sta     $2122
-;    lda     #%00000000  ; Load the high byte of the green color.
-;    sta     $2122
+    lda     #%11100000  ; Load the low byte of the green color.
+    sta     $2122
+    lda     #%00000000  ; Load the high byte of the green color.
+    sta     $2122
 
-;   lda     #%00001111  ; End VBlank, setting brightness to 15 (100%).
-;   sta     $2100
+   lda     #%00001111  ; End VBlank, setting brightness to 15 (100%).
+   sta     $2100
 
     jsr TitleScreen
 
